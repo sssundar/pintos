0d16fba42aa78348537454e15e50d8d09fb2a8ce
dluo11 <dluo@caltech.edu>
2016-01-11 22:45:18 -0800

    modified pipe from readings v2

--- src/shell/pipe.c
+++ src/shell/pipe.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
+#include <sys/wait.h>
+
 // Hardcode some commands
 struct parsedCommand command1;
 struct parsedCommand command2;
 
-int pipe(int filedes[2]);
+// int pipe(int filedes[2]);
 
 int main (void)
 {
@@ -11,18 +13,47 @@ int main (void)
   int fd[2];
   pid_t pid;
   char line[MAXLINE];
+  FILE *fp;
 
   if (pipe(fd) < 0)
     err_sys("pipe error");
   if ((pid = fork()) < 0) {
     err_sys("fork error");
   } else if (pid  > 0) { /* parent */
+    // close read end
     close(fd[0]);
-    write(fd[1], "hello world\n", 12);
-  } else { /* child */
+
+    while (fgets(line, MAXLINE, fp) != NULL) {
+      n = strlen(line);
+      if (write(fd[1], line, n) != n)
+        err_sys("write error to pipe");
+    }
+    if (ferror(fp))
+      err_sys("fgets error");
     close(fd[1]);
-    n = read(fd[0], line, MAXLINE);
-    write(STDOUT_FILENO, line, n);
+
+    if (waitpid(pid, NULL, 0) < 0)
+      err_sys("waitpid error");
+    exit();
+    
+  } else { /* child */
+    // close write end
+    close(fd[1]);
+
+    if (fd[O] != STDIN_FILENO) {
+      if (dup2(fd[O], STDIN_FILENO) != STDIN_FILENO)
+        err_sys("dup2 error to stdin");
+
+      /*get arguments for execl() */
+      if ((pager = getenv("PAGER")) == NULL)
+        pager = DEF_PAGER;
+      
+      if ((argvO = strrchr(pager, '/')) !=NULL)
+        argvO++; /* step past rightmost slash */
+      else
+        argvO = pager; /* no slash in pager */
+      if (execl(pager, argvO, (char *)0) < 0)
+        err_sys("execl error for %s", pager); 
   }
   exit(0);
   
