10ed4661a54f4758a325108b4239f457b79536a3
Hamik Mukelyan <hamik@caltech.edu>
2016-01-20 14:06:28 -0800

    Keyboard circular queue written.

--- src/booter/keyboard.c
+++ src/booter/keyboard.c
@@ -1,6 +1,9 @@
+#include <stdint.h>
 #include "ports.h"
+#include "interrupts.h"
 
-/* This is the IO port of the PS/2 controller, where the keyboard's scan
+/**
+ * This is the IO port of the PS/2 controller, where the keyboard's scan
  * codes are made available.  Scan codes can be read as follows:
  *
  *     unsigned char scan_code = inb(KEYBOARD_PORT);
@@ -21,8 +24,10 @@
  *
  * See http://wiki.osdev.org/PS/2_Keyboard for details.
  */
-#define KEYBOARD_PORT 0x60
+#define KEYBOARD_PORT 	0x60
 
+/** Length of the circular keyboard buffer (needs to be less than 8 bits) */
+#define KEYBUFLEN 		100
 
 /* TODO:  You can create static variables here to hold keyboard state.
  *        Note that if you create some kind of circular queue (a very good
@@ -36,12 +41,62 @@
  *        so that nothing gets mangled...
  */
 
+/* Circular buffer of scan codes */
+static volatile uint8_t kbuf[KEYBUFLEN];
+
+/* Index to the current head in the circular scan-code buffer. */
+static volatile int start;
+
+/* Index to the current tail in the circular scan-code buffer. */
+static volatile int end;
+
+/**
+ * Enqueues the given scan code in the circular buffer.
+ *
+ * @param scode One byte of a scan code.
+ */
+void enqueue(uint8_t scode) {
+	kbuf[end] = scode;
+	// Full queue case: overwrite the item that was enqueued longest ago by
+	// writing over the element at "start". Increase "end" and "start" by 1
+	// modulo n.
+	if ((end + 1) % KEYBUFLEN == start) {
+		start = (start + 1) % KEYBUFLEN;
+	}
+	end = (end + 1) % KEYBUFLEN;
+}
+
+/**
+ * Dequeues a scan code from the circular buffer.
+ *
+ * @return The scan code from the tail of the circular buffer or zero if the
+ * buffer is empty.
+ */
+uint8_t dequeue() {
+	uint8_t rtn;
+	// Empty queue case:
+	if (start == end) {
+		rtn = 0;
+	}
+	// Non-empty queue case:
+	else {
+		rtn = kbuf[start];
+                kbuf[start] = 0;
+		start = (start + 1) % KEYBUFLEN;
+	}
+	return rtn;
+}
 
 void init_keyboard(void) {
     /* TODO:  Initialize any state required by the keyboard handler. */
+	start = 0;
+	end = 0;
 
     /* TODO:  You might want to install your keyboard interrupt handler
      *        here as well.
      */
 }
+
+
+
 
--- src/booter/keyboard.h
+++ src/booter/keyboard.h
@@ -3,5 +3,20 @@
 
 void init_keyboard(void);
 
+/**
+ * Enqueues the given scan code in the circular buffer.
+ *
+ * @param scode One byte of a scan code.
+ */
+void enqueue(uint8_t scode);
+
+/**
+ * Dequeues a scan code from the circular buffer.
+ *
+ * @return The scan code from the tail of the circular buffer or zero if the
+ * buffer is empty.
+ */
+uint8_t dequeue();
+
 #endif /* KEYBOARD_H */
 
