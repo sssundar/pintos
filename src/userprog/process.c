c2e4e32bc6e88a5b95640e4d9e81636a5d3ede8f
Sushant Sundaresh <sssundar@caltech.edu>
2016-02-08 21:54:31 -0800

    temporary argc fix

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -404,7 +404,11 @@ static bool setup_stack(void **esp) {
     if (kpage != NULL) {
         success = install_page(((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
         if (success)
-            *esp = PHYS_BASE;
+            /* Original Code
+            *esp = PHYS_BASE;   */
+
+            /* ==TODO== Temporary fix until argument passing is implemented */
+            *esp = PHYS_BASE - 12;
         else
             palloc_free_page(kpage);
     }
